04c307b66d169ad671c1474e10a2a5c66e27b136
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2025-06-26 23:18:14 +0500

    Understanding Types In Java

---
+++ Java Basics/src/PracticeQuiz01.java
@@ -0,0 +1,15 @@
+//This quiz is from the starting concept of java in which I have learn about types of datatypes,
+// how to declare a variable primitive Datatypes and different types of assignment and unary operators.
+import java.util.*;
+import java.lang.String;
+public class PracticeQuiz01 {
+    public static void main(String[] args) {
+        byte num = 1;
+        String Happy = "Why you are happy?";
+        int Age = 18;
+        Age = 19;
+        System.out.println(Age);
+        System.out.println(Happy);
+    }
+}
+
